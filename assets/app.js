97c9f603b2c15122d4a93513fa4620287dbc3b71
Yusuke Wada <yusuke@kamawada.com>
2016-04-15 11:38:28 +0900

    add .js file.

---
+++ assets/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from './components/button';
+
+class App extends React.Component {
+  render () {
+    return (
+      <Button/>
+    );
+  }
+}
+
+export default App;
